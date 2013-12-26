bbb20ce5b2c5a67f0dfe33bfedf07f71baeab1d2
Mike Coutermarsh <mcoutermarsh@gazelle.com>
2013-12-26 17:41:01 -0500

    word count solution

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,9 +1,10 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase
+    #lower case and remove none word characters
+    @normalized_phrase = phrase.downcase.gsub(/[^a-zA-Z0-9-'\s]+/, " ")
   end
 
   def word_count
-    @phrase.split.inject(Hash.new(0)) { |c,w| c[w] +=1; c }
+    @normalized_phrase.split.inject(Hash.new(0)) { |c,w| c[w] +=1; c }
   end
 end
