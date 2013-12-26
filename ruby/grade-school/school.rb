2a5db2e30524ee0c126a101c60885acc1f55914d
Mike Coutermarsh <mcoutermarsh@gazelle.com>
2013-12-26 00:35:05 -0500

    use hash instead of array to store students

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -1,28 +1,23 @@
 class School
   def initialize
-    @students = Array.new(13) { Array.new }
+    @students = Hash.new{ |a,b| a[b] = Array.new }
   end
 
   def add(name, grade)
     @students[grade] << name
   end
 
-  def db
-    output = {}
-
-    @students.each_with_index do |students, grade|
-      output[grade] = students if students.size > 0 
-    end
-
-    output
-  end
-
   def sort
-    db.each { |grade, students| students.sort! }
+    @students.each { |grade, students| students.sort! }
+    Hash[@students.sort]
   end
 
   def grade(grade)
     @students[grade]
+  end
+
+  def db
+    @students
   end
 
 end
