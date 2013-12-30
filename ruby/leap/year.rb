518b7e1f593253f4d84c5af0d00bbe2ace98144f
Mike Coutermarsh <mcoutermarsh@gazelle.com>
2013-12-30 12:50:25 -0500

    iteration on year

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -4,19 +4,10 @@ class Year
   end
 
   def leap?
-    return true if divisible_by?(400)
-    return true if divisible_by?(4) && !divisible_by?(100)
-    false
+    divisible_by?(400) || (divisible_by?(4) && !divisible_by?(100))
   end
 
   def divisible_by?(i)
     @year % i == 0
   end
 end
-
-
-# ```plain
-# on every year that is evenly divisible by 4
-#   except every year that is evenly divisible by 100
-#     unless the year is also evenly divisible by 400
-# ```
