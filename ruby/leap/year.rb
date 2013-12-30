ee13e4a5930d3996ed731c806888d54f67d90ad7
Mike Coutermarsh <mcoutermarsh@gazelle.com>
2013-12-30 13:13:28 -0500

    year iteration

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -4,7 +4,7 @@ class Year
   end
 
   def leap?
-    divisible_by?(400) || (divisible_by?(4) && !divisible_by?(100))
+    divisible_by?(4) && !divisible_by?(100) || divisible_by?(400)
   end
 
   def divisible_by?(i)
