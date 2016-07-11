4030277d3453b2bece426f06686e13a0a8c0e951
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2016-07-10 21:30:05 -0700

    Solution to word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,30 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
+    word_list = sentence
+                |> remove_punctuation()
+                |> replace_underscores()
+                |> String.downcase()
+                |> String.split()
 
+    words_with_count(word_list, %{})
+  end
+
+  defp words_with_count([word | rest_of_list], word_tally) do
+    current_value = word_tally[word] || 0
+
+    words_with_count(rest_of_list, Map.put(word_tally, word, current_value + 1))
+  end
+
+  defp words_with_count([], map) do
+    map
+  end
+
+  defp remove_punctuation(sentence) do
+    String.replace(sentence, ~r/[:!&@$%^,]/, "")
+  end
+
+  defp replace_underscores(sentence) do
+    String.replace(sentence, ~r/_/, " ")
   end
 end
